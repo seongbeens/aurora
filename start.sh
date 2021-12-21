6adaaeddd167e1fe53e2bb4eb2a42709cacfb870
seongbeens <bsung6512@gmail.com>
2021-12-21 22:39:45 +0900

    MODIFY start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 cd /home/ubuntu/aurora-lastest
 
-while true; do
-	ISRUN=`pgrep -f "sudo python3 app.py" | wc -l`
-	if [ ${ISRUN} -eq 0 ]; then
-		nohup sudo python3 app.py > logs/nohup_app.out &
-	fi
+ISRUN=`pgrep -f "start.sh" | wc -l`
+if [ ${ISRUN} -eq 0 ]; then
+	while true; do
+		ISRUN=`pgrep -f "sudo python3 app.py" | wc -l`
+		if [ ${ISRUN} -eq 0 ]; then
+			nohup sudo python3 app.py > logs/nohup_app.out &
+		fi
 
-	ISRUN=`pgrep -f "sudo python3 schedules.py" | wc -l`
-	if [ ${ISRUN} -eq 0 ]; then
-		nohup sudo python3 schedules.py > logs/nohup_schedules.out &
-	fi
-	sleep 2
-done
+		ISRUN=`pgrep -f "sudo python3 schedules.py" | wc -l`
+		if [ ${ISRUN} -eq 0 ]; then
+			nohup sudo python3 schedules.py > logs/nohup_schedules.out &
+		fi
+		sleep 2
+	done
+else
+	echo "start.sh: ALREADY RUNNING"
+fi
