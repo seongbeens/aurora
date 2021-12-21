02991e1a874bce01280bf6c69cab67c1a2ce8c86
seongbeens <bsung6512@gmail.com>
2021-12-21 22:00:50 +0900

    HOTFIX myssql.py

--- mysql.py
+++ mysql.py
@@ -13,10 +13,9 @@ def createAccount(chat_id):
   try:
     conn = connect()
     cur = conn.cursor()
-    # if cur.execute("SELECT * FROM Accounts WHERE telegram_id = '{}'".format(str(chat_id))) == 0:
     cur.execute(
-      "INSERT INTO Accounts SET telegram_id = '{}' ".format(chat_id)\
-    + "ON DUPLICATE KEY UPDATE date_recreate = current_timestamp")
+      "INSERT INTO Accounts SET telegram_id = %d "\
+    + "ON DUPLICATE KEY UPDATE date_recreate = current_timestamp", (chat_id))
     conn.commit()
     return True
   
@@ -33,8 +32,7 @@ def modifyAccount(chat_id, columns, tuples):
     cur = conn.cursor()
     for i, j in zip(columns, tuples):
       cur.execute(
-        "INSERT INTO Accounts SET telegram_id = '{}', {} = '{}' ".format(chat_id, i, j)\
-      + "ON DUPLICATE KEY UPDATE {} = '{}'".format(i, j))
+        "UPDATE Accounts SET {} = %s WHERE telegram_id = %s".format(i), (j, chat_id))
     conn.commit()
     return True
   
@@ -55,7 +53,7 @@ def deleteAccount(chat_id):
     cur.execute(
       "UPDATE Accounts SET nickname = NULL, phone = NULL, email = NULL, "\
     + "token_refresh = NULL, token_access = NULL, vehicle_counts = NULL, default_vehicle = NULL "\
-    + "WHERE telegram_id = '{}'".format(chat_id))
+    + "WHERE telegram_id = %s", (chat_id))
     conn.commit()
     return True
 
@@ -72,7 +70,7 @@ def inquiryAccount(chat_id, columns): # 구문 변경
     cur = conn.cursor()
     cur.execute(
       "SELECT {} FROM Accounts ".format(', '.join(columns))\
-    + "WHERE telegram_id = '{}'".format(chat_id))
+    + "WHERE telegram_id = %s", (chat_id))
     return cur.fetchone()
   
   except Exception as e:
@@ -106,12 +104,10 @@ def createVehicle(chat_id, veh_id, veh_name, vin):
   try:
     conn = connect()
     cur = conn.cursor()
-    # if cur.execute("SELECT * FROM accounts WHERE telegram_id = '{}'".format(str(chat_id))) == 0:
     cur.execute(
       "INSERT INTO Vehicles SET "\
-    + "telegram_id = '{}', vehicle_id = '{}', ".format(chat_id, veh_id)\
-    + "vehicle_name = '{}', vin = '{}' ".format(veh_name, vin)\
-    + "ON DUPLICATE KEY UPDATE vehicle_name = '{}', vin = '{}' ".format(veh_name, vin))
+    + "telegram_id = %s, vehicle_id = %s, vehicle_name = %s, vin = %s "\
+    + "ON DUPLICATE KEY UPDATE vehicle_name = %s, vin = %s", (chat_id, veh_id, veh_name, vin, veh_name, vin))
     conn.commit()
     return True
 
@@ -128,9 +124,8 @@ def modifyVehicle(chat_id, veh_id, columns, tuples):
     cur = conn.cursor()
     for i, j in zip(columns, tuples):
       cur.execute(
-        "INSERT INTO Vehicles SET "\
-      + "telegram_id = '{}', vehicle_id = '{}', {} = '{}' ".format(chat_id, veh_id, i, j)\
-      + "ON DUPLICATE KEY UPDATE {} = '{}'".format(i, j))
+        "UPDATE Vehicles SET {} = %s ".format(i)\
+      + "WHERE telegram_id = %s AND vehicle_id = %s", (j, chat_id, veh_id))
     conn.commit()
     return True
   
@@ -148,11 +143,11 @@ def deleteVehicle(chat_id, veh_id = None):
     if veh_id:
       cur.execute(
         "DELETE FROM Vehicles "\
-      + "WHERE telegram_id = '{}' AND vehicle_id = '{}'".format(chat_id, veh_id))
+      + "WHERE telegram_id = %s AND vehicle_id = %s", (chat_id, veh_id))
     else:
       cur.execute(
         "DELETE FROM Vehicles "\
-      + "WHERE telegram_id = '{}'".format(chat_id))
+      + "WHERE telegram_id = %s", (chat_id))
     conn.commit()
     return True
   
@@ -170,12 +165,12 @@ def inquiryVehicle(chat_id, veh_id, columns): # 구문 변경
     if veh_id:
       cur.execute(
         "SELECT {} FROM Vehicles ".format(', '.join(columns))\
-      + "WHERE telegram_id = '{}' AND vehicle_id = {}".format(chat_id, veh_id))
+      + "WHERE telegram_id = %s AND vehicle_id = %s", (chat_id, veh_id))
       return cur.fetchone()
     else:
       cur.execute(
         "SELECT {} FROM Vehicles ".format(', '.join(columns))\
-      + "WHERE telegram_id = '{}'".format(chat_id))
+      + "WHERE telegram_id = %s", (chat_id))
       return cur.fetchall()
 
   except Exception as e:
